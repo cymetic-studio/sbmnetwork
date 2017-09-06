b971f75e365cc715a40ba1733b1dff45c1841938
adorufus <adorufus@gmail.com>
2017-09-06 08:44:56 +0700

    adding file

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bhakti Mulya Network</title>
+        <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/fix-jumbotron.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/magnific-popup/magnific-popup.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+    </head>
+    <body id="page-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#">SBM NETWORK </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="#">Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Berita</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Perpustakaan Online</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header class="masthead">
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 class="fix-fontsize" id="homeHeading">Bhakti Mulya Network</h1>
+                <p>Untukmu Bhaktiku Kumuliakan</p>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <h2 class="section-heading">Kenapa Harus Bhakti Mulya?</h2>
+                    <hr class="light">
+                    <p class="text-dark">Karena sekolah ini adalah sekolah dengan mutu pendidikan yang baik dan mampu menghasilkan lulusan - lulusan yang kompeten di dunia kerja.</p>
+                    <a class="btn btn-default btn-xl js-scroll-trigger" href="#services">Lebih Banyak</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="{{asset('assets/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/popper/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+
+    <script src="{{asset('assets/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('assets/scrollreveal/scrollreveal.min.js')}}"></script>
+    <script src="{{asset('assets/magnific-popup/magnific-popup.min.js')}}"></script>
+
+    <script src="{{asset('assets/js/app.js')}}"></script>
+    </body>
+</html>
---
+++ resources/views/news.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Berita</title>
+    </head>
+    <body>
+        <h1>ini adalah halaman berita</h1>
+    </body>
+</html>
